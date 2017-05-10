ea05ff175c19e60f1a806370ba97999e3c94dee7
Stephen Bailey <StephenBailey@lbl.gov>
2017-05-10 14:05:53 -0700

    refactored to check return codes

--- mpitest.py
+++ mpitest.py
@@ -19,12 +19,6 @@ import json
 
 def blat(i, j):
     print('blat({}, {})'.format(i, j))    
-    filename = 'mpitest-{}-{}.txt'.format(i, j)
-    assert not os.path.exists(filename)
-    fx = open(filename, 'w')
-    fx.write('hello {} {}'.format(i, j))
-    fx.close()
-    assert os.path.exists(filename)
 
 #-------------------------------------------------------------------------
 print('Rank {} is alive'.format(comm.rank))
@@ -46,36 +40,19 @@ if len(sys.argv) > 1 and sys.argv[1] == '--hang':
 nproc = 4
 proclist = list()
 for j in range(nproc):
-    filename = 'mpitest-{}-{}.txt'.format(comm.rank, j)
-    if os.path.exists(filename):
-        os.remove(filename)
-
-    #- Works to call blat directly, but not within a spawned process
-    # blat(comm.rank, j, qin, qout)
-    print('Rank {} starting process {}'.format(comm.rank, j))
     p = multiprocessing.Process(target=blat, args=[comm.rank, j])
     p.start()
     proclist.append(p)
 
-comm.barrier()
-
-#- Doesn't help
-for j in range(nproc):
-    p = proclist[j]
-    p.join()
-    print('Rank {} proc {} pid {} exit {}'.format(comm.rank, j, p.pid, p.exitcode))
-    print('Rank {} finished process {}'.format(comm.rank, j))
-
-comm.barrier()
 sys.stdout.flush()
 comm.barrier()
 
-if comm.rank == 0:
-    for i in range(comm.size):
+#- Check return codes
+for i in range(comm.size):
+    if i == comm.rank:
         for j in range(nproc):
-            filename = 'mpitest-{}-{}.txt'.format(i, j)
-            if os.path.exists(filename):
-                print('OK   {} {}'.format(i, j))
-                os.remove(filename)
-            else:
-                print('FAIL {} {}'.format(i, j))
+            p = proclist[j]
+            p.join()
+            print('Rank {} proc {} pid {} exit {}'.format(comm.rank, j, p.pid, p.exitcode))
+        sys.stdout.flush()
+    comm.barrier()
